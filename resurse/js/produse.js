260f504f5e627c21d2b3e31cbdb22885fb4e1500
Stefan Popa <popashtefan10@gmail.com>
2022-05-08 00:36:35 +0300

    Filtru de tip textarea

--- resurse/js/produse.js
+++ resurse/js/produse.js
@@ -13,8 +13,28 @@ function getStringFromNormalized(normalizedString) {
 	return retString;
 }
 
+function ascundeDotari() {
+	const LIM = 6;
+
+	var dotari = document.getElementsByClassName("container-dotare");
+	for (let idx = LIM; idx < dotari.length; idx++)
+	dotari[idx].style.display = "none";
+
+	var btnAscundeDotari = document.getElementById("ascunde-dotari").querySelector("button");
+	btnAscundeDotari.innerHTML = "+ Mai multe";
+}
+
+function afiseazaDotari() {
+	var dotari = document.getElementsByClassName("container-dotare");
+	for (let idx = 0; idx < dotari.length; idx++)
+	dotari[idx].style.display = "block";
+
+	var btnAscundeDotari = document.getElementById("ascunde-dotari").querySelector("button");
+	btnAscundeDotari.innerHTML = "- Mai putine";
+}
+
 window.onload = function () {
-	// functieTest();
+	ascundeDotari();
 
 	// ascunde filtrele
 	document.getElementById("ascunde-filtre").addEventListener('click', function() {
@@ -26,32 +46,41 @@ window.onload = function () {
 			sectiuneFiltre.style.display = "none";
 	});
 
-    // butonul de filtrare
-    document.getElementById("filtrare").addEventListener('click', function () {
-        // obtin valorile dupa care sa filtrez
-        // nume
-        var valNume = document.getElementById("inp-nume").value.toLowerCase();
+	// ascunde dotarile
+	document.getElementById("ascunde-dotari").addEventListener('click', function() {
+		var btnAscundeDotari = document.getElementById("ascunde-dotari").querySelector("button");
+		if (btnAscundeDotari.innerHTML[0] == "+")
+			afiseazaDotari();
+		else
+			ascundeDotari();
+	});
 
-        // interval cai putere
-        var butoaneRadio = document.getElementsByName("gr_rad");
-        for (let butonRadio of butoaneRadio)
-            if (butonRadio.checked)
-                var valCaiPutere = butonRadio.value;
-    
-        var minCaiPutere, maxCaiPutere; // obtin intervalul de cai putere
-        if (valCaiPutere != "toate") {
-            [minCaiPutere, maxCaiPutere] = valCaiPutere.split(":")
-            minCaiPutere = parseInt(minCaiPutere);
-            maxCaiPutere = parseInt(maxCaiPutere);
-        }
-        else
-            minCaiPutere = 0, maxCaiPutere = 10000;
+	// butonul de filtrare
+	document.getElementById("filtrare").addEventListener('click', function () {
+		// obtin valorile dupa care sa filtrez
+		// nume
+		var valNume = document.getElementById("inp-nume").value.toLowerCase();
 
-        // pret minim
-        var minPret = document.getElementById("inp-pret").value;
+		// interval cai putere
+		var butoaneRadio = document.getElementsByName("gr_rad");
+		for (let butonRadio of butoaneRadio)
+			if (butonRadio.checked)
+				var valCaiPutere = butonRadio.value;
+	
+		var minCaiPutere, maxCaiPutere; // obtin intervalul de cai putere
+		if (valCaiPutere != "toate") {
+			[minCaiPutere, maxCaiPutere] = valCaiPutere.split(":")
+			minCaiPutere = parseInt(minCaiPutere);
+			maxCaiPutere = parseInt(maxCaiPutere);
+		}
+		else
+			minCaiPutere = 0, maxCaiPutere = 10000;
 
-        // tip caroserie
-        var tipCaroserie = document.getElementById("inp-categorie").value;
+		// pret minim
+		var minPret = document.getElementById("inp-pret").value;
+
+		// tip caroserie
+		var tipCaroserie = document.getElementById("inp-categorie").value;
 
 		// dotari
 		var checkboxDotari = document.getElementsByName("dotari");
@@ -61,22 +90,26 @@ window.onload = function () {
 				dotariAlese.push(checkboxDotare.value);
 		}
 
-        // filtrarea produselor
-        var produse = document.getElementsByClassName("produs");
-        for (let produs of produse) {
-            produs.style.display = "none";
+		// cuvinte cheie
+		var textareaCuvCheie = document.getElementById("textarea-cuv-cheie");
+		var cuvinteCheie = textareaCuvCheie.value.split(",").map(cuvCheie => cuvCheie.trim());
 
-            var numeProdus = produs.getElementsByClassName("val-nume")[0].innerHTML.toLowerCase();
-            var condNume = numeProdus.includes(valNume);
+		// filtrarea produselor
+		var produse = document.getElementsByClassName("produs");
+		for (let produs of produse) {
+			produs.style.display = "none";
 
-            var caiPutereProdus = parseInt(produs.getElementsByClassName("val-cai-putere")[0].innerHTML);
-            var condCaiPutere = caiPutereProdus >= minCaiPutere && caiPutereProdus < maxCaiPutere;
+			var numeProdus = produs.getElementsByClassName("val-nume")[0].innerHTML.toLowerCase();
+			var condNume = numeProdus.includes(valNume);
 
-            var pretProdus = parseInt(produs.getElementsByClassName("val-pret")[0].innerHTML);
-            var condPret = pretProdus >= minPret;
+			var caiPutereProdus = parseInt(produs.getElementsByClassName("val-cai-putere")[0].innerHTML);
+			var condCaiPutere = caiPutereProdus >= minCaiPutere && caiPutereProdus < maxCaiPutere;
 
-            var tipCaroserieProdus = produs.getElementsByClassName("val-categorie")[0].innerHTML;
-            var condTipCaroserie = tipCaroserie == "toate" || tipCaroserieProdus == tipCaroserie;
+			var pretProdus = parseInt(produs.getElementsByClassName("val-pret")[0].innerHTML);
+			var condPret = pretProdus >= minPret;
+
+			var tipCaroserieProdus = produs.getElementsByClassName("val-categorie")[0].innerHTML;
+			var condTipCaroserie = tipCaroserie == "toate" || tipCaroserieProdus == tipCaroserie;
 
 			var dotariProdus = produs.getElementsByClassName("val-dotari")[0].innerHTML.split(",");
 			var condDotari = false;
@@ -87,87 +120,100 @@ window.onload = function () {
 					dotareProdus = dotareProdus.trim();
 					condDotari ||= dotariAlese.includes(dotareProdus);
 				}
-			// condDotari = true;
+			
+			var descriereProdus = produs.getElementsByClassName("descriere")[0].innerHTML.toLowerCase();
+			var condDescriere = false;
+			for (let cuvantCheie of cuvinteCheie)
+				condDescriere ||= descriereProdus.includes(cuvantCheie);
 
-            var condFinala = condNume && condCaiPutere && condPret && condTipCaroserie && condDotari;
-            if (condFinala)
-                produs.style.display = "block";
-        };
-    });
+			var condFinala =
+				condNume &&
+				condCaiPutere &&
+				condPret &&
+				condTipCaroserie &&
+				condDotari &&
+				condDescriere;
+			if (condFinala)
+				produs.style.display = "block";
+		};
+	});
 
-    // afisez valoarea pe care o indica range-ul
-    document.getElementById("inp-pret").addEventListener("input", function() {
-        document.getElementById("infoRange").innerHTML = `(${document.getElementById("inp-pret").value})`;
-    });
+	// afisez valoarea pe care o indica range-ul
+	document.getElementById("inp-pret").addEventListener("input", function() {
+		document.getElementById("infoRange").innerHTML = `(${document.getElementById("inp-pret").value})`;
+	});
 
-    // butonul de resetare
-    document.getElementById("resetare").addEventListener("click", function() {
-        // resetez filtrele la starea initiala
-        document.getElementById("inp-nume").value = "";
-        document.getElementById("i_rad5").checked = true;
-        document.getElementById("inp-pret").value = document.getElementById("inp-pret").defaultValue;
-        document.getElementById("infoRange").innerHTML = `(${document.getElementById("inp-pret").defaultValue})`;
-        document.getElementById("sel-toate").selected = true;
+	// butonul de resetare
+	document.getElementById("resetare").addEventListener("click", function() {
+		// resetez filtrele la starea initiala
+		document.getElementById("inp-nume").value = "";
+		document.getElementById("i_rad5").checked = true;
+		document.getElementById("inp-pret").value = document.getElementById("inp-pret").defaultValue;
+		document.getElementById("infoRange").innerHTML = `(${document.getElementById("inp-pret").defaultValue})`;
+		document.getElementById("sel-toate").selected = true;
+		document.getElementById("textarea-cuv-cheie").value = "";
 
+		// dotari
 		var checkboxuriDotari = document.getElementsByName("dotari");
 		for (let checkboxDotare of checkboxuriDotari)
 			checkboxDotare.checked = false;
+		ascundeDotari();
 
-        // afisez toate produsele
-        var produse = document.getElementsByClassName("produs");
-        for (let produs of produse) {
-            produs.style.display = "block";
-        }
-    });
+		// afisez toate produsele
+		var produse = document.getElementsByClassName("produs");
+		for (let produs of produse) {
+			produs.style.display = "block";
+		}
+	});
 
-    // butonul de sortare crescator dupa pret si nume
-    document.getElementById("sortCrescNume").addEventListener("click", function() {
-        var produse = document.getElementsByClassName("produs");
-        var vProduse = Array.from(produse);
+	// butonul de sortare crescator dupa pret si nume
+	document.getElementById("sortCrescNume").addEventListener("click", function() {
+		var produse = document.getElementsByClassName("produs");
+		var vProduse = Array.from(produse);
 
-        vProduse.sort((a, b) => {
-            return cmpProduse(a, b, [{functieCmp: cmpProdusePret, ascending: true}, {functieCmp: cmpProduseNume, ascending: true}]);
-        });
+		vProduse.sort((a, b) => {
+			return cmpProduse(a, b, [{functieCmp: cmpProdusePret, ascending: true}, {functieCmp: cmpProduseNume, ascending: true}]);
+		});
 
-        for(let produs of vProduse) {
-            produs.parentElement.appendChild(produs);
-        }
-    });
+		for(let produs of vProduse) {
+			produs.parentElement.appendChild(produs);
+		}
+	});
 
-    // butonul de sortare descrescator dupa pret si nume
-    document.getElementById("sortDescrescNume").addEventListener("click", function() {
-        var produse = document.getElementsByClassName("produs");
-        var vProduse = Array.from(produse);
+	// butonul de sortare descrescator dupa pret si nume
+	document.getElementById("sortDescrescNume").addEventListener("click", function() {
+		var produse = document.getElementsByClassName("produs");
+		var vProduse = Array.from(produse);
 
-        vProduse.sort((a, b) => {
-            return cmpProduse(a, b, [{functieCmp: cmpProdusePret, ascending: false}, {functieCmp: cmpProduseNume, ascending: false}]);
-        });
+		vProduse.sort((a, b) => {
+			return cmpProduse(a, b, [{functieCmp: cmpProdusePret, ascending: false}, {functieCmp: cmpProduseNume, ascending: false}]);
+		});
 
-        for(let produs of vProduse) {
-            produs.parentElement.appendChild(produs);
-        }
-    });
+		for(let produs of vProduse) {
+			produs.parentElement.appendChild(produs);
+		}
+	});
 };
 
 // functii de comparare de produse dupa diferite criterii
 function cmpProdusePret(produsA, produsB, ascending) {
-    var pretA = parseInt(produsA.getElementsByClassName("val-pret")[0].innerHTML);
-    var pretB = parseInt(produsB.getElementsByClassName("val-pret")[0].innerHTML);
-    return ascending ? pretA - pretB : pretB - pretA;
+	var pretA = parseInt(produsA.getElementsByClassName("val-pret")[0].innerHTML);
+	var pretB = parseInt(produsB.getElementsByClassName("val-pret")[0].innerHTML);
+	return ascending ? pretA - pretB : pretB - pretA;
 }
 
 function cmpProduseNume(produsA, produsB, ascending) {
-    var numeA = produsA.getElementsByClassName("val-nume")[0].innerHTML.toLowerCase();
-    var numeB = produsB.getElementsByClassName("val-nume")[0].innerHTML.toLowerCase();
-    return (ascending ? 1 : -1) * numeA.localeCompare(numeB);
+	var numeA = produsA.getElementsByClassName("val-nume")[0].innerHTML.toLowerCase();
+	var numeB = produsB.getElementsByClassName("val-nume")[0].innerHTML.toLowerCase();
+	return (ascending ? 1 : -1) * numeA.localeCompare(numeB);
 }
 
 function cmpProduse(produsA, produsB, vObFunctiiCmp) {
-    for(let obFunctieCmp of vObFunctiiCmp) {
-        var rezFunctieCmp = obFunctieCmp.functieCmp(produsA, produsB, obFunctieCmp.ascending);
-        if (rezFunctieCmp)
-            return rezFunctieCmp;
-    }
+	for(let obFunctieCmp of vObFunctiiCmp) {
+		var rezFunctieCmp = obFunctieCmp.functieCmp(produsA, produsB, obFunctieCmp.ascending);
+		if (rezFunctieCmp)
+			return rezFunctieCmp;
+	}
 
-    return -1;
+	return -1;
 }
